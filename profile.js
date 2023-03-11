bdc82025c456e1423e6715ed7df394d8487e6504
lerover1155@gmail.com <lerover1155@gmail.com>
2023-03-12 01:17:09 +0630

    Profile add

---
+++ profile.js
@@ -0,0 +1,8 @@
+const followerEl = document.querySelector('.follower_amount')
+let follower = 0
+followerEl.textContent= follower
+function addfollower() {
+    follower = follower + 1
+followerEl.textContent = follower
+
+}
