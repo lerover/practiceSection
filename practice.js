7db88cbcbc05268b396553b76f4e440a48ae27a5
lerover1155@gmail.com <lerover1155@gmail.com>
2023-03-11 11:57:06 +0630

    changeABit

--- practice.js
+++ practice.js
@@ -1,23 +1,30 @@
+let people = []
 
 let count = 0
 const counting_number = document.querySelector('.counting_number')
 counting_number.innerText = count
 
 function increase() {
-count = count + 1
- counting_number.innerText= count
+    count = count + 1
+    counting_number.innerText = count
 }
 
 function decrease() {
     count = count - 1
-     counting_number.innerText= count
-    }
+    counting_number.innerText = count
+}
+
+function save() {
+    const newArray = people.push(count)
+    console.log(people)
+}
 
 function reset() {
- count = count - count 
- counting_number.innerText = count
-    
+    count = count - count
+    counting_number.innerText = count
+
 }
+
 
 
 
