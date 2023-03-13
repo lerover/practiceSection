987c40d165143eb6663972c8b3711784fc30ef9e
lerover1155@gmail.com <lerover1155@gmail.com>
2023-03-13 10:29:55 +0630

    addTwoFiles

--- profile.js
+++ profile.js
@@ -1,8 +1,20 @@
 const followerEl = document.querySelector('.follower_amount')
+const follow_changeEl = document.querySelector('.follow_change')
 let follower = 0
 followerEl.textContent= follower
 function addfollower() {
     follower = follower + 1
 followerEl.textContent = follower
+follow_changeEl.textContent= 'Liked' 
+if(follower < 2) {
+    follower = 2-1
+        followerEl.textContent = follower
+}
+}
 
+function touch_again_unfollow() {
+    follower = follower - 1
+    followerEl.textContent = follower
+    follow_changeEl.textContent= 'follow'
+    
 }
